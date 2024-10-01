47ae03b1dd362658146da951bb5626fd627147fc
TL <Tommy.Lin@wnc.com.tw>
2024-10-01 19:13:40 +0800

    Merge dev into master

--- app/main.py
+++ app/main.py
@@ -74,4 +74,4 @@ async def get_html():
         return HTMLResponse(content=f.read(), status_code=200)
     
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
