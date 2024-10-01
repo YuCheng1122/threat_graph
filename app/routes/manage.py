adefe04c1ed4b10b3efbc377780dce1a9e21adb6
TL <Tommy.Lin@wnc.com.tw>
2024-10-01 19:37:57 +0800

    adjust license get api function

--- app/routes/manage.py
+++ app/routes/manage.py
@@ -6,6 +6,7 @@ from api_web_dev.app.schemas.manage import GroupListResponse, GroupEmailMap, App
 from api_web_dev.app.ext.error import UnauthorizedError, InternalServerError
 from app.controllers.auth import AuthController
 from logging import getLogger
+from app.models.user_db import UserModel
 from app.schemas.manage import TotalAgentsAndLicenseResponse, UserListResponse, UserInfo
 from app.schemas.user import UserSignup
 from app.models.manage_db import SessionLocal
@@ -61,6 +62,32 @@ async def update_license(request: UpdateLicenseRequest, user: UserModel = Depend
         raise
     except Exception as e:
         logger.error(f"Unexpected error in {update_license.__name__}: {e}")
+        raise InternalServerError()
+
+@router.get("/total-agents-and-license", response_model=TotalAgentsAndLicenseResponse)
+async def get_total_agents_and_license(user: UserSignup = Depends(AuthController.get_current_user)):
+    """Get the total number of agents and the total license amount"""
+    try:
+
+        if user.user_role == 'admin':
+            group_names = None
+        else:
+            group_names = UserModel.get_user_groups(user.id)
+            logger.info(f"User groups: {group_names}")
+            if not group_names:
+                logger.warning(f"No groups found for user {user.id}")
+                return TotalAgentsAndLicenseResponse(total_agents=0, total_license=0)
+
+        total_agents = await ManageController.get_total_agents(group_names)
+        total_license = ManageController.get_total_license(user.id if user.user_role != 'admin' else None)
+        logger.info(f"Total agents: {total_agents}, Total license: {total_license}")
+
+        return TotalAgentsAndLicenseResponse(total_agents=total_agents, total_license=total_license)
+    except UnauthorizedError as ue:
+        logger.error(f"Unauthorized error for user {user.id}: {str(ue)}")
+        raise
+    except Exception as e:
+        logger.error(f"Unexpected error in {get_total_agents_and_license.__name__} for user {user.id}: {e}")
         raise InternalServerError()
 
 @router.get("/users", response_model=UserListResponse)
